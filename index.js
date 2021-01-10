176d0b90b9adf168cc7876ce65a3a8c1835683ba
R4GHAV <67642579+R4GHAV@users.noreply.github.com>
2021-01-10 22:11:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navv").style.backgroundColor = 'rgba('+13+','+ 13+','+ 13+','+ 1+')';
+  } else {
+    document.getElementById("navv").style.backgroundColor = 'rgba('+13+','+ 13+','+ 13+','+ 0.01+')';
+  }
+}
+function mute() {
+  var video=document.querySelector('.vid');
+  if(video.muted === true){
+    video.muted=false;
+    document.querySelector('.mute').innerHTML="<i class=\"fas fa-volume-up\" aria-hidden=\"true\"></i>";
+  }else if (video.muted === false) {
+    video.muted=true;
+      document.querySelector('.mute').innerHTML="<i class=\"fas fa-volume-mute\" aria-hidden=\"true\"></i>";
+  }
+}
